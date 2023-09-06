602ad3a71b01410f06b6da2931b63c84c41477b3
popstarmav <cj.johnson2017@gmail.com>
2023-09-05 19:00:23 -0700

    Print an interger

---
+++ python-hello_world/3-print_number.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# This Python scripty prints an interger followed by:
+# "Battery Street"
+# The integer is stored in the variable "number."
+
+number = 98
+print(f'{number} Battery street')
