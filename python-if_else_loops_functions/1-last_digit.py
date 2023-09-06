e81f4dadab30357ad5c67b0246ccc4f88acddf14
popstarmav <cj.johnson2017@gmail.com>
2023-09-06 16:46:20 -0700

    Print last string

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,12 +8,12 @@ number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 
 # Determine the message based on the last digit
-if last_digit > 5:
-    message = "and is greater than 5"
+if last_digit > 0:
+    message = "negative"
 elif last_digit == 0:
-    message = "and is 0"
+    message = "zero"
 else:
-    message = "and is less than 6 and not 0"
+    message = "positive"
 
 # Print the final message
 print(f"The string Last digit of {number} is {last_digit} {message}")
