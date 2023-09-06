9156a3c4062f42b3d3291af18487e1f9bc4a1868
popstarmav <cj.johnson2017@gmail.com>
2023-09-06 07:27:17 -0700

    preclearscript

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-# This is a Python scripts that prints an integer followed by:
-# "Battery street"
-# The integer is stored in the variable:
-# "number"
-
-
 number = 98
-message = f'{number} Battery street'
+message = f'\'{number} Battery street'
 print(message)
