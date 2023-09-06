30f3d03a20767e72cd5e9465de5e9adf82bfe4a4
popstarmav <cj.johnson2017@gmail.com>
2023-09-05 18:26:33 -0700

    Updated version

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-#This is Python script that print a message
-#Using the print function to display "Programming is like building a multilingual puzzle"
+# This is Python script that print a message
+# Print function to display:
+# "Programming is like building a multilingual puzzle"
 
 print("Programming is like building a multilingual puzzle")
