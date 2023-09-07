16b37b04d8f2cc885da6ee8daa45117924c2b64c
popstarmav <cj.johnson2017@gmail.com>
2023-09-06 19:40:55 -0700

    python project

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 
-# Import the add function from add_0.py
-from add_0 import add
-
 if __name__ == "__main__":
-    # Define variables a and b
+    """Print the sum of 1 and 2."""
+    from add_0 import add
+
     a = 1
     b = 2
-
-    # Calculate the result using the add function
-    result = add(a, b)
-
-    # Print the result using string formatting
-    print("{} + {} = {}" .format(a, b, result))
+    print("{} + {} = {}".format(a, b, add(a, b)))
