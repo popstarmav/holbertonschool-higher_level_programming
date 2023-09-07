9e1aa994168c44fe6758332fcfa4f1bfe429d503
popstarmav <cj.johnson2017@gmail.com>
2023-09-06 18:09:22 -0700

    Program that imoort tge function from the file add_0.py

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Import the add function from add_0.py
+from add_0 import add
+
+# Assign values to variables a and b
+a = 1
+b = 2
+
+# Calculate the result using the imported add function
+result = add(a, b)
+
+# Print the result with string formatting
+print(f"{a} + {b} = {result}")
