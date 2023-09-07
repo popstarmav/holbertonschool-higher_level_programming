bd98914d7ed8f88b8fc3414c088f7424286cf818
popstarmav <cj.johnson2017@gmail.com>
2023-09-06 17:03:52 -0700

    Print ABC

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    print(chr(i), end='')
-
-print()  # To add a newline at the end
+print(''.join(chr(i) for i in range(ord('a'), ord('z') + 1)), end='')
