a5d3ef15a814fca591944a0c242a00d89586ba87
popstarmav <cj.johnson2017@gmail.com>
2023-09-07 09:51:50 -0700

    Print Caculation

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -6,7 +6,4 @@ from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
 
-print(f"{a} + {b} = {add(a, b)}")
-print(f"{a} - {b} = {sub(a, b)}")
-print(f"{a} * {b} = {mul(a, b)}")
-print(f"{a} / {b} = {div(a, b)}")
+print(f"{a} + {b} = {add(a, b)}"'\n'f"{a} - {b} = {sub(a, b)}"'\n'f"{a} * {b} = {mul(a, b)}"'\n'f"{a} / {b} = {div(a, b)}")
