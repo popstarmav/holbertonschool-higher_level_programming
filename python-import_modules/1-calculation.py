9db72ec64e2dbfa4d709e4f6660a5005ba2e5932
popstarmav <cj.johnson2017@gmail.com>
2023-09-07 12:10:04 -0700

    Print Cacluator

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -6,12 +6,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    addition_result = add(a, b)
-    subtraction_result = sub(a, b)
-    multiplication_result = mul(a, b)
-    division_result = div(a, b)
-
-    print(f"{a} + {b} = {addition_result}")
-    print(f"{a} - {b} = {subtraction_result}")
-    print(f"{a} * {b} = {multiplication_result}")
-    print(f"{a} / {b} = {division_result}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
