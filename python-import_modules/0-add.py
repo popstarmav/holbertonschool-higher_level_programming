3132e6d1f411d6b6a0564c443a9fedf82334a01e
popstarmav <cj.johnson2017@gmail.com>
2023-09-06 18:25:25 -0700

    Program that imoort tge function from the file add_0.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 
 if __name__ == "__main__":
     from add_0 import add
@@ -6,5 +7,6 @@ if __name__ == "__main__":
     a = 1
     b = 2
     result = add(a, b)
-    print(f"{a} + {b} = {result}")
+    
+    print("{} + {} = {}".format(a, b, result))
 
