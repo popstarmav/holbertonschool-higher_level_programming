2b975ff15d575f4613e4a791a377670acad7f695
popstarmav <cj.johnson2017@gmail.com>
2023-09-06 21:32:31 -0700

    print combo

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,8 @@
 
 for digit1 in range(10):
     for digit2 in range(digit1 + 1, 10):
-        print("{:d}{:d}".format(digit1, digit2), end=", " if digit2 < 9 else "\n")
+        if digit1 == 8 and digit2 == 9:
+            print("{:d}{:d}".format(digit1, digit2))
+        else:
+            print("{:d}{:d}, ".format(digit1, digit2), end="")
+
