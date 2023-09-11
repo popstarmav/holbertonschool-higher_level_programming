e56c4317c291e5785d37242caaa9e90aa76a5342
popstarmav <cj.johnson2017@gmail.com>
2023-09-11 14:54:39 -0700

    remove c an C

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,10 +3,8 @@
 def no_c(my_string):
     # Initialize an empty string to store the result
     result = ""
-    
     # Iterate through each character in the input string
     for char in my_string:
         if char != 'c' and char != 'C':
-            result += char
-    
+            result += char 
     return result
