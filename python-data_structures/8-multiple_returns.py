fb11c1d36d2690047d70fdcf123d25b3b1a65b0d
popstarmav <cj.johnson2017@gmail.com>
2023-09-11 16:23:32 -0700

    print tuple with len w str

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
-#!/usr/bin/pythpn3
+#!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if len(sentence) > 0:
-        return (len(sentence), sentence[0])
-    else:
+    if not sentence:
         return (0, None)
+    return (len(sentence), sentence[0])
