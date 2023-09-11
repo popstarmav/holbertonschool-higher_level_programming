dcc0e1b2c8bafed49887fd85394d734816720f9c
popstarmav <cj.johnson2017@gmail.com>
2023-09-11 15:05:08 -0700

    print a maxtix of int

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
-def print_matrix_integer(matrix=[[]]):
+def print_matrix_integer(matrix=[]):
+    if not matrix:
+        return  # Return if the matrix is empty:
     for row in matrix:
-        for num in row:
-            print("{:d}".format(num), end=" ")
+        for i, num in enumerate(row):
+            print("{:d}".format(num), end=" " if i < len(row) - 1 else "")
         print()
