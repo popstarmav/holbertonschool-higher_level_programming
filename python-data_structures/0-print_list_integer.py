2bf63341bb51b64bac14bc796ca3ec617964d1c1
popstarmav <cj.johnson2017@gmail.com>
2023-09-11 10:55:08 -0700

    prints all integers

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# 0-print_list_integer.py
 
 def print_list_integer(my_list=[]):
-    print("\n".join(map(str, my_list)))
+    for i in range(len(my_list)):
+        print("{}".format(my_list))
