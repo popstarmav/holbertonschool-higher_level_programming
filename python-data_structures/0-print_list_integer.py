6defca5fc5983a81cb958b4b0c242c6101e1bc89
popstarmav <cj.johnson2017@gmail.com>
2023-09-11 11:21:07 -0700

    print list

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
- for elements in my_list:
-        print(elements, end="\n")
+    if isinstance(my_list, list):
+        for num in my_list:
+            print("{:d}".format(num))
