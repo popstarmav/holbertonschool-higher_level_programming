deb1a89b29a95eda4afd8303879e89bc45b83867
popstarmav <cj.johnson2017@gmail.com>
2023-09-11 11:09:41 -0700

    print list

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    for num in my_list:
-        if isinstance(num, int):  # Check if the element is an integer
-            print("{:d}".format(num))
-        else:
-            raise TypeError("List should only contain integers")
+ for elements in my_list:
+        print(elements, end="\n")
