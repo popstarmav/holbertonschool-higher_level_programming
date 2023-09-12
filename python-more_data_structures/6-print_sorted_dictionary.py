772cde98a127bf64359a0d95e6e60e87e5503b8b
popstarmav <cj.johnson2017@gmail.com>
2023-09-11 20:57:05 -0700

    sorted keys

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    # Sort the keys alphabetically
+    sorted_keys = sorted(a_dictionary.keys())
+
+    # Iterate through sorted keys and print key-value pairs
+    for key in sorted_keys:
+        value = a_dictionary[key]
+        print(f"{key}: {value}")
