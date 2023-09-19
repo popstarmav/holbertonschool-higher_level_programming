0f59ae007addcc31e860677c7854abd12bb5fc6f
popstarmav <cj.johnson2017@gmail.com>
2023-09-19 11:07:30 -0700

    defines a rectangle

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""Rectangle module.
-
-This define a rectangle
 
 """
+This is a Python module for handling rectangles.
 
+This module defines a basic Rectangle class to represent rectangles.
+"""
 
-class Rectangle():
-    """Represent a rectangle"""
+class Rectangle:
+    """
+    A class to represent rectangles.
 
+    This class provides a basic structure to work with rectangles.
+    It does not contain any specific attributes or methods yet.
+    """
     pass
