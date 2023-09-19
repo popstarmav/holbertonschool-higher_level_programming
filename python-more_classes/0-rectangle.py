9bfba2a07a21d63eb28e6fd1c4f670a69b805d4e
popstarmav <cj.johnson2017@gmail.com>
2023-09-19 11:48:56 -0700

    DEFINE EMPTY RECTANGEL

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,19 +1,5 @@
 #!/usr/bin/python3
 
 class Rectangle:
-    def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
-
-    def area(self):
-        """Calculate the area of the rectangle."""
-        return self.width * self.height
-
-    def perimeter(self):
-        """Calculate the perimeter of the rectangle."""
-        return 2 * (self.width + self.height)
-
-    def __str__(self):
-        """Return a string representation of the rectangle."""
-        return f"Rectangle (width={self.width}, height={self.height})"
+    pass
 
