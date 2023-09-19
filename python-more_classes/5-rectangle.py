031dd05e5adaa92fbebc415e0b52ca6326972608
popstarmav <cj.johnson2017@gmail.com>
2023-09-19 13:35:30 -0700

    define rec based by 4-rec

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -1,9 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/python3 
 """
-Defines a rectangle base on 4-rec
+Defines a rectangle based in 4-rec
 """
 
 class Rectangle:
+    """ defines variables & methods """
+
     def __init__(self, width=0, height=0):
         self._width = width
         self._height = height
@@ -14,11 +16,12 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        if isinstance(value, int) and value >= 0:
+            self._width = value
+        elif not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
-        self._width = value
 
     @property
     def height(self):
@@ -26,28 +29,34 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
+        if isinstance(value, int) and value >= 0:
+            self._height = value
+        elif not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
-        self._height = value
 
     def area(self):
-        return self.width * self.height
+        return self._width * self._height
 
     def perimeter(self):
-        if self.width == 0 or self.height == 0:
-            return 0
-        return 2 * (self.width + self.height)
+        return (self._width + self._height) * 2
 
     def __str__(self):
-        if self.width == 0 or self.height == 0:
-            return ""
-        rectangle_str = ""
-        for _ in range(self.height):
-            rectangle_str += "#" * self.width + "\n"
-        return rectangle_str[:-1]  # Remove the trailing newline
+        pattern = ""
+        if self._width == 0 or self._height == 0:
+            return pattern
+        else:
+            for j in range(self._height):
+                for i in range(self._width):
+                    pattern += "#"
+                if j != self._height - 1:
+                    pattern += "\n"
+            return pattern
 
     def __repr__(self):
-        return f"Rectangle({self.width}, {self.height})"
+        return f"Rectangle({self._width}, {self._height})"
+
+    def __del__(self):
+        print("Bye rectangle...")
 
