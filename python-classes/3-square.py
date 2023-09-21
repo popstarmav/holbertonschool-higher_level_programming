a0628a2cde0aa768404fa9edf02633fec0c9ddcc
popstarmav <cj.johnson2017@gmail.com>
2023-09-20 19:45:48 -0700

    defines square based by 2-squ

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -6,12 +6,12 @@ Defines square based by 2-square
 
 class Square:
     """defines variables and method"""
-
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         return self.__size ** 2
