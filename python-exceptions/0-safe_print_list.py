410a0a57afcf957fd7df3fc73ad96d8ccb4c9cee
popstarmav <cj.johnson2017@gmail.com>
2023-09-20 21:24:09 -0700

    print x element list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,33 @@
 #!/usr/bin/python3
+"""
+This function print the element of
+"""
+
 
 def safe_print_list(my_list=[], x=0):
+    """ defines methods and variables """
+
     try:
-        for i in range(x):
-            print(my_list[i], end=' ')
-    except IndexError:
-        pass
-    finally:
+        if x < 0:
+            x = 0
+
+        count = 0
+        for item in my_list:
+            if count < x:
+                print(item, end=" ")
+                count += 1
+            else:
+                break
+
         print()
-    
-    return min(x, len(my_list))
+
+        return count
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return 0
+
+my_list = [1, 2, 3, 4, 5]
+x = 3
+printed_count = safe_print_list(my_list, x)
+print(f"Printed {printed_count} elements")
