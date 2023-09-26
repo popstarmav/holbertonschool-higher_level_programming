d81968f435e8100dd91b6754ae0460bce4aeaddb
popstarmav <cj.johnson2017@gmail.com>
2023-09-26 05:40:55 -0700

    print true

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -3,6 +3,7 @@
 Function return true
 """
 
+
 def is_kind_of_class(obj, a_class):
     """
     Check if obj is an instance of a_class or its subclass.
@@ -12,4 +13,3 @@ def is_kind_of_class(obj, a_class):
     :return: True if obj is a_class or its subclass, False otherwise.
     """
     return isinstance(obj, a_class)
-
