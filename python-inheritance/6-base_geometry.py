44c6f4903f3251ae262da760809d5f881b04c19f
popstarmav <cj.johnson2017@gmail.com>
2023-09-26 15:27:38 -0700

    CLass Basegeometry

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,16 +1,6 @@
 #!/usr/bin/pyhton3
-"""
-BaseGeometry base on 5-base_goe
-"""
-
-
 class BaseGeometry:
-    """
-    A class representing basic geometry operations.
-
-    Public instance method:
-        - area(self): Raises an Exception with a message indicating it's not implemented.
-    """
+    """Class of BaseGeometry"""
     def area(self):
         """
         Raise an exception indicating that the area method is not implemented.
