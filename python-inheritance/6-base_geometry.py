0ca5d0e7cb8bf3adbcf750570ff56cad6b17beff
popstarmav <cj.johnson2017@gmail.com>
2023-09-26 15:12:31 -0700

    CLass Basegeometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/pyhton3
+"""
+BaseGeometry base on 5-base_goe
+"""
+
+
+class BaseGeometry:
+    """ class area """
+    def area(self):
+        raise Exception("area() is not implemented")
+
