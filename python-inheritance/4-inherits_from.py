ad4f1f157d3cecd6b8854c58352be2a996651225
popstarmav <cj.johnson2017@gmail.com>
2023-09-26 06:04:19 -0700

    print true otherwise flase

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,11 +5,13 @@ Function print true or false
 
 
 def inherits_from(obj, a_class):
-    """
-    Check if obj is an instance of a subclass (directly or indirectly)
+    """Check if the object is an instance of a subclass of a_class.
 
-    :param obj: The object to check.
-    :param a_class: The class to compare against.
-    :retrun: True if it's a subclass instance; otherwise, return False..
+    Args:
+        obj: The object to check.
+        a_class: The class to compare against.
+
+    Returns:
+        True if obj is an instance of a subclass, False otherwise.
     """
-    return isinstance(type(obj), a_class)
+    return issubclass(type(obj), a_class) and type(obj) != a_class
