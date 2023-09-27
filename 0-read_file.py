3ecbc11fc73ee16ccf16ec2ff17f425d6c91deb4
popstarmav <cj.johnson2017@gmail.com>
2023-09-26 19:29:28 -0700

    text

--- 0-read_file.py
+++ 0-read_file.py
@@ -6,10 +6,12 @@ it to stdout
 
 def read_file(filename=""):
     try:
-        # Open the file for reading with UTF-8 encoding
-        with open(filename, 'r', encoding='utf-8') as file:
-            # Read and print each line in the file
-            for line in file:
-                print(line, end='')
+        file = open(filename, 'r', encoding='utf-8')
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            print(line, end='')
+        file.close()
     except FileNotFoundError:
-        pass
+        passs
