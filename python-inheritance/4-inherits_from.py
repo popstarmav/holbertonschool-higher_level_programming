71d3ba2bf6e2b2c10cf27fdfeac0529b8c75e7aa
popstarmav <cj.johnson2017@gmail.com>
2023-09-27 08:04:40 -0700

    Only sub class of

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,13 +6,6 @@ Function that returns True or False
 
 def inherits_from(obj, a_class):
     """
-    Confirm if the object belongs to a specified class or its subclasses.
-
-    Args:
-        obj: The object to check.
-        a_class: The class to compare against.
-
-    Returns:
-        True if obj is an instance of a_class or its subclasses, False otherwise.
+    Confirm if the object belongs to a specified class or its subclass
     """
     return issubclass(type(obj), a_class)
