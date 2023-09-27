2a910acba73139b269a70520026e5b522695352a
popstarmav <cj.johnson2017@gmail.com>
2023-09-26 21:47:25 -0700

    Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""adds all arguments to a Python list and save them to file"""
+"""list and save them to file"""
 import sys
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
