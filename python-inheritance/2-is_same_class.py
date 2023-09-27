57889cfdb354b84d6bd8a0b837be6e4ad80374e3
popstarmav <cj.johnson2017@gmail.com>
2023-09-27 07:54:46 -0700

    Exact same object

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -6,10 +6,6 @@ Function return true
 
 def is_same_class(obj, a_class):
     """
-    Verify if obj is of the exact same class as a_class.
-
-    :param obj: The obj being checked.
-    :param a_class: iThe class to compare against.
-    :return: True if obj is of a_class, False otherwise.
+    Verify if obj is of the exact same class as a_class
     """
-    return type(obj) == a_class
+    return type(obj) is a_class
