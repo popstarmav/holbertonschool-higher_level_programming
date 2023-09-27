16fa163faf3a5ca867c6ddb34ba929ce6fb5b28a
popstarmav <cj.johnson2017@gmail.com>
2023-09-26 20:18:53 -0700

    read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,8 +5,8 @@ prints it to stdout
 """
 
 def read_file(filename=""):
+    """Open the file for reading with UTF-8 encoding"""
     try:
-        # Open the file for reading with UTF-8 encoding
         with open(filename, 'r', encoding='utf-8') as file:
             # Read and print each line in the file
             for line in file:
