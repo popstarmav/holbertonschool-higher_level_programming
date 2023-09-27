cdb14f535f4187b0989e6d65e675329a763893c1
popstarmav <cj.johnson2017@gmail.com>
2023-09-26 21:15:20 -0700

    Save Object to a file

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+function that writes an Object 
+to a text file
+"""
+import json
+
+
+
+def save_to_json_file(my_obj, filename):
+    try:
+        with open(filename, 'w', encoding='utf-8') as file:
+            json.dump(my_obj, file)
+    except Exception as e:
+        print(f"An error occurred: {e}")
