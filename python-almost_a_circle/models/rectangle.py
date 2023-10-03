ed0d9b9bc394304286b5e470b1b40051157dcbff
popstarmav <cj.johnson2017@gmail.com>
2023-10-03 15:15:39 -0700

    Almsot a circle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,6 @@
 """
 from models.base import Base
 
-
 class Rectangle(Base):
     """
         Rectangle class that inherits from Base.
@@ -114,8 +113,10 @@ class Rectangle(Base):
         """
             Prints to stdout the Rectangle instance with '#'.
         """
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """
