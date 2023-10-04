e4d045ebb9503078bd0d6a53c8c538771bc4de97
popstarmav <cj.johnson2017@gmail.com>
2023-10-03 19:24:53 -0700

    Student of JSON

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,6 +1,11 @@
 #!/usr/bin/pyhton3
+"""
+Defines the class student
+"""
 
 class Student:
+    """Student instances"""
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
