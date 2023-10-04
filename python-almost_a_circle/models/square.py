395b7ecc4067f3efd8b7d6531c2b9f8d48b8c3f0
popstarmav <cj.johnson2017@gmail.com>
2023-10-03 18:39:59 -0700

    Almost a O

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,47 +1,37 @@
 #!/usr/bin/python3
+import json
+from models.rectangle import Rectangle
 
-class Rectangle:
-    """Rectangle class with width, height, x, and y attributes."""
+class Square(Rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
 
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialize a Rectangle instance.
+    @property
+    def size(self):
+        return self.width
 
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-            x (int): The x-coordinate of the rectangle.
-            y (int): The y-coordinate of the rectangle.
-            id (int): Optional. The ID of the rectangle.
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
 
-        Attributes:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-            x (int): The x-coordinate of the rectangle.
-            y (int): The y-coordinate of the rectangle.
-            id (int): The ID of the rectangle.
-
-        """
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-        if id is not None:
-            self.id = id
+    def update(self, *args, **kwargs):
+        if len(args) > 0:
+            attrs = ['id', 'size', 'x', 'y']
+            for attr, val in zip(attrs, args):
+                setattr(self, attr, val)
         else:
-            Rectangle.__nb_objects += 1
-            self.id = Rectangle.__nb_objects
+            for key, val in kwargs.items():
+                setattr(self, key, val)
 
     def to_dictionary(self):
-        """Return the dictionary representation of a Rectangle.
-
-        Returns:
-            dict: A dictionary containing the attributes of the Rectangle.
-
-        """
         return {
-            "id": self.id,
-            "width": self.width,
-            "height": self.height,
-            "x": self.x,
-            "y": self.y,
+            'id': self.id,
+            'size': self.size,
+            'x': self.x,
+            'y': self.y
         }
