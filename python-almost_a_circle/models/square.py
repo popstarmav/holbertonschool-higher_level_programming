d35d03decf8ba8d08f8de960474cc4dbbc1cc833
popstarmav <cj.johnson2017@gmail.com>
2023-10-04 10:55:19 -0700

    Almost a O

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,50 +1,52 @@
 #!/usr/bin/python3
-# square.py
+"""
+    Contains the Square class that inherits from Rectangle.
+"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """
-        class Square inherits from Rectangle.
+        Represents a square, inherits from Rectangle.
     """
+
     def __init__(self, size, x=0, y=0, id=None):
         """
-            Initializes the instance of the class.
+            Initializes a new Square instance.
+
+            Args:
+                size (int): The size of the square.
+                x (int): The x-coordinate.
+                y (int): The y-coordinate.
+                id (int): The identity of the square.
         """
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
         """
-            getter function for size
-            Returns: size
+            Getter method for size.
+
+            Returns:
+                int: The size of the square.
         """
         return self.width
 
     @size.setter
     def size(self, value):
         """
-            setter function for size
+            Setter method for size.
+
             Args:
-                value (int): value to be set.
+                value (int): The size of the square.
         """
         self.width = value
         self.height = value
 
     def __str__(self):
         """
-            returns a string format of the square
-        """
-        return "[{}] ({}) {}/{} - {}".format(type(self).__name__, self.id,
-                                             self.x, self.y, self.width)
+            Returns a string representation of the square.
 
-    def to_dictionary(self):
+            Format: "[Square] (<id>) <x>/<y> - <size>"
         """
-            returns the dictionary representation of a square
-        """
-        return {
-            'id': self.id,
-            'size': self.width,
-            'x': self.x,
-            'y': self.y
-        }
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
