64e034a7d06736f528b08e21623f0c17154f5fc7
popstarmav <cj.johnson2017@gmail.com>
2023-10-03 17:46:56 -0700

    square.py

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,53 +1,89 @@
-#!/usr/bin/python3
-"""
-Unittests for the Base class.
-"""
-
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+import json
+import os
+
 
 class TestBase(unittest.TestCase):
-    """
-    Test cases for the Base class.
-    """
-
-    def test_init(self):
-        """
-        Test the initialization of the Base class.
-        """
-        b1 = Base()
-        self.assertEqual(b1.id, 1)
-
-        b2 = Base()
-        self.assertEqual(b2.id, 2)
-
-        b3 = Base(12)
-        self.assertEqual(b3.id, 12)
-
-        b4 = Base()
-        self.assertEqual(b4.id, 3)
+    """Test the Base class."""
 
     def test_to_json_string(self):
-        """
-        Test the to_json_string method.
-        """
-        json_str = Base.to_json_string([])
-        self.assertEqual(json_str, "[]")
-
-        json_str = Base.to_json_string(None)
-        self.assertEqual(json_str, "[]")
+        """Test the to_json_string method."""
+        r1 = Rectangle(10, 7, 2, 8)
+        r1_dictionary = r1.to_dictionary()
+        json_string = Base.to_json_string([r1_dictionary])
+        self.assertTrue(isinstance(json_string, str))
+        self.assertEqual(json.loads(json_string), [r1_dictionary])
 
     def test_from_json_string(self):
-        """
-        Test the from_json_string method.
-        """
-        json_str = '[]'
-        lst = Base.from_json_string(json_str)
-        self.assertEqual(lst, [])
+        """Test the from_json_string method."""
+        r1 = Rectangle(10, 7, 2, 8)
+        r1_dictionary = r1.to_dictionary()
+        json_string = Base.to_json_string([r1_dictionary])
+        r2_list = Base.from_json_string(json_string)
+        self.assertTrue(isinstance(r2_list, list))
+        self.assertEqual(r2_list, [r1_dictionary])
 
-        json_str = None
-        lst = Base.from_json_string(json_str)
-        self.assertEqual(lst, [])
+    def test_save_to_file(self):
+        """Test the save_to_file method."""
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
+        with open("Rectangle.json", "r") as file:
+            content = json.load(file)
+        expected = [r1.to_dictionary(), r2.to_dictionary()]
+        self.assertEqual(content, expected)
+
+    def test_load_from_file(self):
+        """Test the load_from_file method."""
+        if os.path.exists("Rectangle.json"):
+            os.remove("Rectangle.json")
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
+        rectangles = Rectangle.load_from_file()
+        self.assertTrue(isinstance(rectangles, list))
+        self.assertEqual(len(rectangles), 2)
+        self.assertTrue(isinstance(rectangles[0], Rectangle))
+        self.assertTrue(isinstance(rectangles[1], Rectangle))
+        self.assertEqual(rectangles[0].to_dictionary(), r1.to_dictionary())
+        self.assertEqual(rectangles[1].to_dictionary(), r2.to_dictionary())
+
+    def test_load_from_file_csv(self):
+        """Test the load_from_file_csv method."""
+        if os.path.exists("Rectangle.csv"):
+            os.remove("Rectangle.csv")
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file_csv([r1, r2])
+        rectangles = Rectangle.load_from_file_csv()
+        self.assertTrue(isinstance(rectangles, list))
+        self.assertEqual(len(rectangles), 2)
+        self.assertTrue(isinstance(rectangles[0], Rectangle))
+        self.assertTrue(isinstance(rectangles[1], Rectangle))
+        self.assertEqual(rectangles[0].to_dictionary(), r1.to_dictionary())
+        self.assertEqual(rectangles[1].to_dictionary(), r2.to_dictionary())
+
+    def test_create(self):
+        """Test the create method of Base class."""
+        r1 = Rectangle(10, 7, 2, 8)
+        r1_dict = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dict)
+        self.assertNotEqual(r1, r2)
+        self.assertNotEqual(r1, r2.__dict__)
+
+    def test_save_to_file_csv(self):
+        """Test the save_to_file_csv method."""
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file_csv([r1, r2])
+        with open("Rectangle.csv", "r") as file:
+            content = file.read()
+        expected = "id,width,height,x,y\n{},10,7,2,8\n{},2,4,0,0\n".format(
+            r1.id, r2.id)
+        self.assertEqual(content, expected)
 
 if __name__ == "__main__":
     unittest.main()
--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-"""
-Unittests for the Rectangle class.
-"""
-
 import unittest
+import io
 from models.rectangle import Rectangle
+from models.base import Base
+
 
 class TestRectangle(unittest.TestCase):
-    """
-    Test cases for the Rectangle class.
-    """
+    """Test cases for the Rectangle class."""
 
     def test_init(self):
-        """
-        Test the initialization of the Rectangle class.
-        """
+        """Test the initialization of the Rectangle class."""
         r1 = Rectangle(10, 20)
         self.assertEqual(r1.width, 10)
         self.assertEqual(r1.height, 20)
@@ -30,9 +25,7 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r2.id, 10)
 
     def test_area(self):
-        """
-        Test the area method of Rectangle.
-        """
+        """Test the area method of Rectangle."""
         r1 = Rectangle(5, 5)
         self.assertEqual(r1.area(), 25)
 
@@ -40,33 +33,27 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r2.area(), 6)
 
     def test_display(self):
-        """
-        Test the display method of Rectangle.
-        """
+        """Test the display method of Rectangle."""
         r1 = Rectangle(2, 2)
         display_output = "##\n##\n"
-        with unittest.mock.patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+        with io.StringIO() as mock_stdout:
             r1.display()
             self.assertEqual(mock_stdout.getvalue(), display_output)
 
         r2 = Rectangle(3, 2, 1, 0)
         display_output = " ###\n ###\n"
-        with unittest.mock.patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+        with io.StringIO() as mock_stdout:
             r2.display()
             self.assertEqual(mock_stdout.getvalue(), display_output)
 
     def test_str(self):
-        """
-        Test the __str__ method of Rectangle.
-        """
+        """Test the __str__ method of Rectangle."""
         r1 = Rectangle(4, 6, 2, 1, 12)
         str_output = "[Rectangle] (12) 2/1 - 4/6"
         self.assertEqual(str(r1), str_output)
 
     def test_update(self):
-        """
-        Test the update method of Rectangle.
-        """
+        """Test the update method of Rectangle."""
         r1 = Rectangle(10, 10, 10, 10)
         r1.update(1, 2)
         self.assertEqual(r1.id, 1)
@@ -83,12 +70,11 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r1.y, 5)
 
     def test_to_dictionary(self):
-        """
-        Test the to_dictionary method of Rectangle.
-        """
+        """Test the to_dictionary method of Rectangle."""
         r1 = Rectangle(10, 20, 30, 40, 50)
         expected_dict = {'id': 50, 'width': 10, 'height': 20, 'x': 30, 'y': 40}
         self.assertEqual(r1.to_dictionary(), expected_dict)
+
 
 if __name__ == "__main__":
     unittest.main()
