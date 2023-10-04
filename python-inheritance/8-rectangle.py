1f4150c50b742a7ba09f86a1c4344461dffc8335
popstarmav <cj.johnson2017@gmail.com>
2023-10-03 21:54:03 -0700

    test

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,46 +1,38 @@
 #!/usr/bin/python3
-"""
-Class Rectangle that inherits from BaseGeometry
-"""
-
-
+"""Rectangle"""
 
 
 class BaseGeometry:
+    """Geometry base class with area() and integer_validator() methods."""
+
     def area(self):
-        raise Exception("area() is not implemented")
+        """Raises 'area not implemented' exception."""
+
+        raise Exception("area() not implemented")
 
     def integer_validator(self, name, value):
+        """Validates 'value' as an integer."""
+
         if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError(f"{name} must be > 0")
 
 
 class Rectangle(BaseGeometry):
+    """Rectangle class that inherits from BaseGeometry."""
+
     def __init__(self, width, height):
-        super().__init__()  # Call the constructor of the base class
+        """Initialize a Rectangle with width and height."""
         self.__width = width
         self.__height = height
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
+
+    def area(self):
+        """Calculate and return the area of the rectangle."""
+        return self.__width * self.__height
 
     def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
-
-if __name__ == "__main__":
-    r = Rectangle(3, 5)
-
-    print(r)
-    print(dir(r))
-
-    try:
-        print("Rectangle: {} - {}".format(r._Rectangle__width, r._Rectangle__height))
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        r2 = Rectangle(4, True)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+        """Return a string representation of the Rectangle object."""
+        return f"Rectangle(width={self.__width}, height={self.__height})"
