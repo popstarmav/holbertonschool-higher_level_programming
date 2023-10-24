1ef729e2dd6eb4a0080f95586d4c8263a10dfb2a
popstarmav <cj.johnson2017@gmail.com>
2023-10-24 11:10:47 +0000

    Read user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,12 +1,9 @@
---creates the database hbtn_0d_2 and the user user_0d_2-- 
--- Create the hbtn_0d_2 database if it doesn't already exist
+--creates the database hbtn_0d_2 and the user user_0d_2
+	--user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
+	--The user_0d_2 password should be set to user_0d_2_pwd
+	--If the database hbtn_0d_2 already exists, your script should not fail
+	--If the user user_0d_2 already exists, your script should not fail
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-
--- Create the 'user_0d_2' user at 'localhost' and set the password
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-
--- Grant SELECT privilege to the 'user_0d_2' on the 'hbtn_0d_2' database
-GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
-
--- Flush privileges to apply the changes
-FLUSH PRIVILEGES;
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
