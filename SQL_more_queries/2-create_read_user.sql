10bf59dbd88b8256d61f6e04e16b641dd78a89fb
popstarmav <cj.johnson2017@gmail.com>
2023-10-24 11:57:44 +0000

    Read user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,7 @@
---creates the database hbtn_0d_2 and the user user_0d_2
--- db = hbtn_0d_2, user = user_0d_2
+-- Create the 'hbtn_0d_2' database if it doesn't exist, 
+-- and establish a 'user_0d_2' with SELECT privileges in 'hbtn_0d_2'.
+-- Set 'user_0d_2's password to 'user_0d_2_pwd'.
+-- The script won't fail if 'hbtn_0d_2' or 'user_0d_2' already exists.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'hbtn_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'hbtn_0d_2'@'localhost';
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
