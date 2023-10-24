6385a6ebf7c9bf1965e77ef3328da3a7bb10951b
popstarmav <cj.johnson2017@gmail.com>
2023-10-24 09:32:10 +0000

    full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,6 +1,16 @@
---  script that creates a table second_table in the database hbtn_0c_0
-iCREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
+USE hbtn_0c_0;
+
+-- Create the second_table if it doesn't already exist
+CREATE TABLE IF NOT EXISTS second_table (
+  id INT,
+  name VARCHAR(256),
+  score INT
+);
+
+-- Insert the records
+INSERT IGNORE INTO second_table (id, name, score)
+VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
