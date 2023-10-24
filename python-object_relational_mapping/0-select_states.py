c3756095003105ca493d72d87f088a7c50ef660e
popstarmav <cj.johnson2017@gmail.com>
2023-10-24 16:43:37 -0700

    get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,12 @@
+t_relational_mapping# cat 0-select_states.
 #!/usr/bin/python3
-"""List all states from a MySQL database."""
+"""List all states from the hbtn_0e_0_usa database."""
 
 import sys
 import MySQLdb
 
 def list_states(username, password, database):
-    """List all states from the given MySQL database.
+    """List all states from the database.
 
     Args:
         username (str): MySQL username
@@ -13,18 +14,16 @@ def list_states(username, password, database):
         database (str): MySQL database name
     """
     # Connect to MySQL server
-    # Connect to MySQL server
     db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
+                host='localhost',
+                port=3306,
+                user=username,
+                passwd=password,
+                db=database
+        )
     cursor = db.cursor()
 
-
-    # Execute SQL query to fetch all states
+    # Execute SQL query to fetch all states sorted by id
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
     # Fetch and print query results
@@ -34,7 +33,7 @@ def list_states(username, password, database):
     # Close the database connection
     db.close()
 
-# Example usage
 if __name__ == '__main__':
-    username, password, database = sys.argv[1:4]
-    list_states(username, password, database)
+    if len(sys.argv) == 4:
+        username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+        list_states(username, password, database)
