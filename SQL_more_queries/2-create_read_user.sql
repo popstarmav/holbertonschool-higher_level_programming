dcfd4f15c5983ee10e1c7c25a3057ea282ae0a1d
popstarmav <cj.johnson2017@gmail.com>
2023-10-24 10:44:59 +0000

    Read user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,9 @@
 --creates the database hbtn_0d_2 and the user user_0d_2-- 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
+
+-- Create the user if it doesn't exist and set the password
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Grant SELECT privileges on the hbtn_0d_2 database to the user
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
