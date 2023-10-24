46ba49a75541e9db57e14aae34b09ca76f1e09d4
popstarmav <cj.johnson2017@gmail.com>
2023-10-24 11:30:22 +0000

    Read user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,9 +1,12 @@
 --creates the database hbtn_0d_2 and the user user_0d_2
-	--user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
-	--The user_0d_2 password should be set to user_0d_2_pwd
-	--If the database hbtn_0d_2 already exists, your script should not fail
-	--If the user user_0d_2 already exists, your script should not fail
-
+-- Create the hbtn_0d_2 database if it doesn't already exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Check if the user already exists
+-- If the user doesn't exist, create it and grant privileges
+SELECT user FROM mysql.user WHERE user = 'user_0d_2';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'%' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'%';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
