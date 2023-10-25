4d1290ebf55d4ed2e6c2b114178d3a0634e2e06f
popstarmav <cj.johnson2017@gmail.com>
2023-10-24 19:40:38 -0700

    get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,41 +1,28 @@
 #!/usr/bin/python3
-"""List all states from the hbtn_0e_0_usa database."""
+"""Lists all states from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 import sys
 
-def list_states(username, password, database):
-    """List all states from the database.
+if __name__ == "__main__":
+    # Connect to the MySQL database
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+    cursor = db.cursor()
 
-    Args:
-        username (str): MySQL username
-        password (str): MySQL password
-        database (str): MySQL database name
-    """
-    try:
-        # Connect to the MySQL database
-        db = MySQLdb.connect(
-            host="localhost",
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            port=3306
-        )
-        cursor = db.cursor()
+    # Execute the SQL query to fetch all states
+    cursor.execute("SELECT * FROM states")
 
-        # Execute the SQL query to fetch all states
-        cursor.execute("SELECT * FROM states")
+    # Fetch and print the results
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
 
-        # Fetch and print the results
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-
-
-    except MySQLdb.Error as e:
-        print(f"Error: {e}")
-
-    finally:
-        # Close the cursor and the database connection
-        cursor.close()
-        db.close()
+    # Close the cursor and the database connection
+    cursor.close()
+    db.close()
