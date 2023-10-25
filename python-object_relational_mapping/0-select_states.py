c7c7800dcbc383bd336cd29f5e2ba9e05e138937
popstarmav <cj.johnson2017@gmail.com>
2023-10-24 21:08:45 -0700

    get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,40 @@
 #!/usr/bin/python3
-"""List all states from the 'hbtn_0e_0_usa' database."""
+"""Lists all states from the database hbtn_0e_0_usa."""
 
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    # Connect to the 'hbtn_0e_0_usa' database
-    database = MySQLdb.connect(
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3]
-    )
-    cursor = database.cursor()
-    cursor.execute("SELECT * FROM states")
-    states = cursor.fetchall()
-    for state in states:
-        print(state)
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    try:
+        # Connect to the MySQL database
+        db = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            passwd=password,
+            db=database_name,
+            port=3306
+        )
+
+        # Create a cursor object to interact with the database
+        cursor = db.cursor()
+
+        # Execute an SQL query to select all states and sort by states.id
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+        # Fetch and print the results
+        states = cursor.fetchall()
+        for state in states:
+            print(state)
+
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+
+    finally:
+        if 'db' in locals() and db:
+            db.close()
