675816dcb169adbff562d158c5e244e1170e31f1
popstarmav <cj.johnson2017@gmail.com>
2023-11-28 05:36:51 -0800

    script that prints “JavaScript is amazing”

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // Constant variable myVar: 'JavaScript is amazing'
-const myVar = '"JavaScript is amazing"';
+const myVar = 'JavaScript is amazing';
 // Using console.log(...) to print the output
 console.log(myVar);
