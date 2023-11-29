4638104f82acb825e016eb54967f57294d7d2215
popstarmav <cj.johnson2017@gmail.com>
2023-11-28 18:27:31 -0800

    print 3 lines

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Using let for variable declaration
+let message1 = "C is fun";
+let message2 = "Python is cool";
+let message3 = "JavaScript is amazing";
+
+// Printing the messages using console.log
+console.log(message1);
+console.log(message2);
+console.log(message3);
