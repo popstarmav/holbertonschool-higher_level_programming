cc541d8cf55dce492d31d4e34738022e088a32ca
popstarmav <cj.johnson2017@gmail.com>
2023-11-28 20:23:58 -0800

    loop to languages

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Using const for constant values
+const languages = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+// Using a for loop to iterate over the array and print each line
+for (const language of languages) {
+  console.log(language);
+}
