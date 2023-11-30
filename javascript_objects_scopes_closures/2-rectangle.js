c28ba7fa2603513b55f3c3d57148b77927c0ef2f
popstarmav <cj.johnson2017@gmail.com>
2023-11-30 00:09:53 -0800

    defines a rectangle:

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,19 +1,10 @@
+#!/usr/bin/node
+
 class Rectangle {
   constructor(width, height) {
-    if (width !== undefined && height !== undefined) {
-      if (width > 0 && height > 0) {
-        this.width = width;
-        this.height = height;
-      } else {
-        this.width = {};
-        this.height = {};
-      }
-    } else if (width !== undefined && height === undefined) {
+    if (width > 0 && height > 0) {
       this.width = width;
-      this.height = {};
-    } else {
-      this.width = {};
-      this.height = {};
+      this.height = height;
     }
   }
 }
