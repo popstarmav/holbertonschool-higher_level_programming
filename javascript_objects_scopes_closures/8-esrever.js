fad2047fa6dca8f2fd8a1cd022429da450f09e43
popstarmav <cj.johnson2017@gmail.com>
2023-11-29 21:14:19 -0800

    returns the reversed version of a list

---
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+function esrever(list) {
+  let start = 0;
+  let end = list.length - 1;
+
+  while (start < end) {
+    const temp = list[start];
+    list[start] = list[end];
+    list[end] = temp;
+    start++;
+    end--;
+  }
+
+  return list;
+}
+
+module.exports = { esrever };
