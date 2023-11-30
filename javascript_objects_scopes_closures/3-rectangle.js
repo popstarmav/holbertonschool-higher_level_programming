0cf23ff04c4ceea6da89ea2fb45650184cfe1fef
popstarmav <cj.johnson2017@gmail.com>
2023-11-29 20:30:06 -0800

    class Rectangle that defines a rectangle

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,6 +1,23 @@
 #!/usr/bin/node
-const validRectangle = new Rectangle(5, 3);
-validRectangle.print();
 
-const invalidRectangle = new Rectangle(0, 4);
-invalidRectangle.print(); // This won't print anything due to the invalid dimensions
+class Rectangle {
+  constructor(width, height) {
+    if (width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    } else {
+      this.width = null;
+      this.height = null;
+    }
+  }
+
+  print() {
+    if (this.width === null || this.height === null) return;
+
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
