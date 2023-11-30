f965e4da20d69c3ce7978163b246428e83f3146d
popstarmav <cj.johnson2017@gmail.com>
2023-11-29 21:03:13 -0800

    defines a square and inherits from Square

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+class Square {
+  constructor(size) {
+    this.size = size;
+  }
+
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.size; i++) {
+      let row = '';
+      for (let j = 0; j < this.size; j++) {
+        row += c;
+      }
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Square;
