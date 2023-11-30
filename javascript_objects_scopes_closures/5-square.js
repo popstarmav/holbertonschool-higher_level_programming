aa63d109e5d3df6c6c4791861933b3d5f31f291f
popstarmav <cj.johnson2017@gmail.com>
2023-11-29 23:09:41 -0800

    inherits from Square of 5-square.js

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,25 +1,11 @@
 #!/usr/bin/node
 
-class Shape {
-  constructor (sideLength) {
-    this.sideLength = sideLength;
-  }
+const Rectangle = require('./4-rectangle');
 
-  print () {
-    if (this.sideLength !== null) {
-      for (let i = 0; i < this.sideLength; i++) {
-        console.log('X'.repeat(this.sideLength));
-      }
-    }
-  }
-
-  double () {
-    if (this.sideLength !== null) {
-      this.sideLength *= 2;
-    }
+class Square extends Rectangle {
+  constructor(size) {
+    super(size, size);
   }
 }
-
-class Square extends Shape {}
 
 module.exports = Square;
