556c39376258de55c3f4bbb8b5f4460b031a8ddb
popstarmav <cj.johnson2017@gmail.com>
2023-11-30 00:00:30 -0800

    Number conversion

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
 
-function createConverter (base) {
-  return function convertToBase (num) {
-    return num.toString(base);
-  };
-}
-
-module.exports = { createConverter };
+const converter = base => num => num.toString(base);
+exports.converter = converter;
