312d5e660410a606c02d58ee16c711e7fef2edc2
popstarmav <cj.johnson2017@gmail.com>
2023-11-29 20:56:54 -0800

    Write a class Square that defines a square

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+class Shape {
+  constructor(sideLength) {
+    this.sideLength = sideLength;
+  }
+
+  print() {
+    if (this.sideLength !== null) {
+      for (let i = 0; i < this.sideLength; i++) {
+        console.log('X'.repeat(this.sideLength));
+      }
+    }
+  }
+
+  double() {
+    if (this.sideLength !== null) {
+      this.sideLength *= 2;
+    }
+  }
+}
+
+class Square extends Shape {}
+
+module.exports = Square;
