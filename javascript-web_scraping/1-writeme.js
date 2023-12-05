619bda2b420d91a7a31a450efe360dfab3f018e6
popstarmav <cj.johnson2017@gmail.com>
2023-12-05 07:41:37 -0600

    write me

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-function writeFileCallback(err) {
-  if (err) {
-    console.log(err);
-  }
-}
-
 // Check if the correct number of arguments is provided
 if (process.argv.length !== 4) {
   console.error('Usage: ./1-writeme.js <file-path> <string-to-write>');
@@ -17,4 +11,4 @@ const filePath = process.argv[2];
 const contentToWrite = process.argv[3];
 
 // Write the content to the file in utf-8
-fs.writeFile(filePath, contentToWrite, writeFileCallback);
+fs.writeFile(filePath, contentToWrite, (error) => console.log(error));
