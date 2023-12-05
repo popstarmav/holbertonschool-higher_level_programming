2eb30d1ab240f54d5c1bff43be17d07bca3b82ba
popstarmav <cj.johnson2017@gmail.com>
2023-12-05 07:24:41 -0600

    write me

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 // Check if the correct number of arguments is provided
 if (process.argv.length !== 4) {
-  console.error('Usage: ./1-write_file.js <file-path> <string-to-write>');
+  console.error('Usage: ./write_file.js <file-path> <string-to-write>');
   process.exit(1); // Exit with an error code
 }
 
@@ -11,9 +11,9 @@ const filePath = process.argv[2];
 const contentToWrite = process.argv[3];
 
 // Write the content to the file in utf-8
-fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
-  if (err) {
-    console.error(err); // Print the error object
+fs.writeFile(filePath, contentToWrite, 'utf-8', (error) => {
+  if (error) {
+    console.error(error); // Print the error object
   } else {
     console.log(`Content successfully written to ${filePath}`);
   }
