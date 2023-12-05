2a4f4b23adad25813cae1182f60bd0262e0ec5c4
popstarmav <cj.johnson2017@gmail.com>
2023-12-05 07:31:21 -0600

    write me

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,6 +1,16 @@
 #!/usr/bin/node
 const fs = require('fs');
 
+function writeToFile(filePath, contentToWrite) {
+  fs.writeFile(filePath, contentToWrite, 'utf-8', (error) => {
+    if (error) {
+      console.error(error); // Print the error object
+    } else {
+      console.log(`Content successfully written to ${filePath}`);
+    }
+  });
+}
+
 // Check if the correct number of arguments is provided
 if (process.argv.length !== 4) {
   console.error('Usage: ./write_file.js <file-path> <string-to-write>');
@@ -10,20 +20,5 @@ if (process.argv.length !== 4) {
 const filePath = process.argv[2];
 const contentToWrite = process.argv[3];
 
-// Write the content to the file in utf-8
-fs.writeFile(filePath, contentToWrite, 'utf-8', (error) => {
-  if (error) {
-    console.error(error); // Print the error object
-  } else {
-    console.log(`Content successfully written to ${filePath}`);
-  }
-});
-
-// Read the content of the file to verify the output
-fs.readFile(filePath, 'utf-8', (readError, readData) => {
-  if (readError) {
-    console.error(`Error reading the file: ${readError}`);
-  } else {
-    console.log('Correct output - text:', JSON.stringify(readData));
-  }
-});
+// Call the function to write to the file
+writeToFile(filePath, contentToWrite);
