840544bbc61c6359da6b427855ab4deedb63c2bf
popstarmav <cj.johnson2017@gmail.com>
2023-12-05 23:32:28 -0600

    status code

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`Status code: ${response.statusCode}`);
+});
