fdc785c34139a73f07db95e1450ce9fab5dc1b5f
popstarmav <cj.johnson2017@gmail.com>
2023-12-05 23:41:07 -0600

    starwars

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 
 const request = require('request');
-const movieId = process.argv[2];
-const API_URL = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+const episodeNum = process.argv[2];
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
 
-request(API_URL, function (err, response, body) {
+request(API_URL + episodeNum, function (err, response, body) {
   if (err) {
     console.error(err);
   } else if (response.statusCode === 200) {
-    const movieData = JSON.parse(body);
-    console.log(movieData.title);
+    const responseJSON = JSON.parse(body);
+    console.log(responseJSON.title);
   } else {
-    console.error(`Error code: ${response.statusCode}`);
+    console.error('Error code: ' + response.statusCode);
   }
 });
